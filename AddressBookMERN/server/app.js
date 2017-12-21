77156128b719ebe1849c2de936d78f3657469eeb
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-12-21 22:23:30 +0100

    React FullStack

--- AddressBookMERN/server/app.js
+++ AddressBookMERN/server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const contactRouter = require('./routes/contact');
 const notFound = require('./middlewares/not-found');
 const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
 app.use('/api/contacts', contactRouter);
 
 app.use('/api', notFound);
